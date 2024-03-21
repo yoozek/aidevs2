f676d7547a9b5b435fff77e28ae7ff9e927b33c1
Lukasz Jozwik <ext.lukasz.jozwik@softwareone.com>
2024-03-22 00:00:26 +0100

    Init

---
+++ AiDevs2.Tasks/AiDevsApiClient.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Text.Json;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AiDevs2.Tasks;
+
+public class AiDevsApiClient
+{
+    private readonly HttpClient _httpClient;
+    private readonly string _apiKey;
+
+    public AiDevsApiClient(HttpClient httpClient, IConfiguration configuration)
+    {
+        _httpClient = httpClient;
+        _apiKey = configuration["AiDevsTasks:ApiKey"] ?? throw new InvalidOperationException("Missing configurationAiDevsTasks:ApiKey");
+    }
+
+    public async Task<string> GetAuthenticationToken(string taskName)
+    {
+        var response = await _httpClient.PostAsync($"https://tasks.aidevs.pl/token/{taskName}",
+            new StringContent(JsonSerializer.Serialize(new { apikey = _apiKey }), Encoding.UTF8, "application/json"));
+        response.EnsureSuccessStatusCode();
+
+        var content = await response.Content.ReadAsStringAsync();
+        var json = JsonSerializer.Deserialize<JsonElement>(content);
+        return json.GetProperty("token").GetString() ?? throw new InvalidOperationException("No token in response");
+    }
+
+    public async Task<string> GetTask(string token)
+    {
+        var response = await _httpClient.GetAsync($"https://tasks.aidevs.pl/task/{token}");
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadAsStringAsync();
+    }
+
+    public async Task SubmitAnswer(string token, string answer)
+    {
+        var response = await _httpClient.PostAsync($"https://tasks.aidevs.pl/answer/{token}",
+            new StringContent(JsonSerializer.Serialize(new { answer }), Encoding.UTF8, "application/json"));
+
+        Console.WriteLine(response.IsSuccessStatusCode
+            ? "OK"
+            : "ERROR");
+    }
+}
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddAiDevsApiClient(this IServiceCollection services)
+    {
+        services.AddHttpClient<AiDevsApiClient>();
+    }
+}
---
+++ AiDevs2.Tasks/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AiDevs2.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var configurationBuilder = new ConfigurationBuilder();
+        configurationBuilder
+            .AddJsonFile("appsettings.json", false);
+
+        var configuration = configurationBuilder.Build();
+
+        IServiceCollection services = new ServiceCollection();
+        services.AddAiDevsApiClient();
+        services.AddSingleton<IConfiguration>(configuration);
+        var container = services.BuildServiceProvider();
+        
+        var client = container.GetRequiredService<AiDevsApiClient>();
+        var taskName = "helloapi";
+        var token = await client.GetAuthenticationToken(taskName);
+        var task = await client.GetTask(token);
+        Console.WriteLine(task);
+    }
+
+}
