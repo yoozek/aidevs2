faf105c2c9fe142cf3dcb266264dca3f7bc735cb
Lukasz Jozwik <ext.lukasz.jozwik@softwareone.com>
2024-03-27 20:04:21 +0100

    rozwiązanie do Embedding

---
+++ AiDevs2.Tasks/Tasks/Embedding.cs
@@ -0,0 +1,23 @@
+﻿using AiDevs2.Tasks.ApiClients;
+using Azure.AI.OpenAI;
+using Microsoft.Extensions.Logging;
+
+namespace AiDevs2.Tasks.Tasks;
+
+public class Embedding(AiDevsClient aiDevsClient, OpenAIClient openAiClient, ILogger<HelloApi> logger)
+    : AiDevsTaskBase("embedding", aiDevsClient, logger)
+{
+    public override async Task Run()
+    {
+        await GetTask();
+
+        var response = openAiClient.GetEmbeddingsAsync(new EmbeddingsOptions
+        {
+            DeploymentName = "text-embedding-ada-002",
+            Input = { "Hawaiian pizza" }
+        });
+
+        var vectors = response.Result.Value.Data.Select(x => x.Embedding).ToArray();
+        await SubmitAnswer(vectors[0]);
+    }
+}
