05ccddf0380376aeaed053b7d8bb8e84e84eb9e4
Lukasz Jozwik <ext.lukasz.jozwik@softwareone.com>
2024-04-17 00:14:43 +0200

    OptimalDb task

---
+++ AiDevs2.Tasks/Tasks/OptimalDb.cs
@@ -0,0 +1,16 @@
+﻿using AiDevs2.Tasks.ApiClients;
+using AiDevs2.Tasks.Tasks.Common;
+using Microsoft.Extensions.Logging;
+
+namespace AiDevs2.Tasks.Tasks;
+
+public class OptimalDb(AiDevsClient aiDevsClient, ILogger<AiDevsTaskBase> logger) 
+    : AiDevsTaskBase("optimaldb", aiDevsClient, logger)
+{
+    public override async Task Run()
+    {
+        var task = await GetTask();
+
+        await SubmitAnswer(await File.ReadAllTextAsync("Tasks/OptimalDbTask/db.json"));
+    }
+}
